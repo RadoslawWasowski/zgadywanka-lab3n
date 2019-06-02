bcec0bb1026216be290921518a104671b3625b32
Radosław Wąsowski <tojaradekw@gmail.com>
2019-06-02 13:46:05 +0200

    Create GraHistoria.cs

---
+++ GraZaDuzoZaMalo/ModelGry/GraHistoria.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelGry
+{
+	public partial class Gra
+	{
+        private readonly List<Ruch> historia;
+        public IReadOnlyList<Ruch> Historia => historia;
+
+
+        public class Ruch
+        {
+            public int propozycja;
+            public Odpowiedz odpowiedz;
+            public DateTime kiedy;
+
+            public Ruch(int prop, Odpowiedz odp)
+            {
+                propozycja = prop;
+                odpowiedz = odp;
+                kiedy = DateTime.Now;
+            }
+            
+            public overrride string ToString()
+            {
+                return $"({propozycja}, {odpowiedz}, {kiedy})";
+            }
+        }
+
+	}
+}
